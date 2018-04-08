58a4a41ea40e73c507b7656101772dc02f9c2587
György Balássy <balassy@users.noreply.github.com>
2018-04-08 15:15:38 +0200

    REMOVED: Deleted the unused files from the Web project, generated by the Visual Studio template.

--- Web/ClientApp/app/app.shared.module.ts
+++ Web/ClientApp/app/app.shared.module.ts
@@ -7,29 +7,23 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
-    ],
-    imports: [
-        CommonModule,
-        HttpModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: '**', redirectTo: 'home' }
+    ])
+  ]
 })
 export class AppModuleShared {
 }

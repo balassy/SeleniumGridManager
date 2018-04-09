563634466fed9fc7d7b2c03bed55a100a63daa51
György Balássy <balassy@users.noreply.github.com>
2018-04-09 05:56:16 +0200

    CHANGED: JSON responses are formatted with indentation.

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -8,53 +8,60 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Newtonsoft.Json;
+
 namespace SeleniumGridManager.Web
 {
-	public class Startup
-	{
-		public IConfiguration Configuration { get; }
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
 
 
-		public Startup( IConfiguration configuration )
-		{
-			this.Configuration = configuration;
-		}
+    public Startup( IConfiguration configuration )
+    {
+      this.Configuration = configuration;
+    }
 
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices( IServiceCollection services )
-		{
-			services.AddMvc();
-		}
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices( IServiceCollection services )
+    {
+      services
+        .AddMvc()
+        .AddJsonOptions( options =>
+         {
+           options.SerializerSettings.Formatting = Formatting.Indented;
+         } );
+    }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure( IApplicationBuilder app, IHostingEnvironment env )
-		{
-			if( env.IsDevelopment() )
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseWebpackDevMiddleware( new WebpackDevMiddlewareOptions
-				{
-					HotModuleReplacement = true
-				} );
-			}
-			else
-			{
-				app.UseExceptionHandler( "/Home/Error" );
-			}
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure( IApplicationBuilder app, IHostingEnvironment env )
+    {
+      if( env.IsDevelopment() )
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseWebpackDevMiddleware( new WebpackDevMiddlewareOptions
+        {
+          HotModuleReplacement = true
+        } );
+      }
+      else
+      {
+        app.UseExceptionHandler( "/Home/Error" );
+      }
 
-			app.UseStaticFiles();
+      app.UseStaticFiles();
 
-			app.UseMvc( routes =>
-			 {
-				 routes.MapRoute(
-									name: "default",
-									template: "{controller=Home}/{action=Index}/{id?}" );
+      app.UseMvc( routes =>
+      {
+        routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}" );
 
-				 routes.MapSpaFallbackRoute(
-									name: "spa-fallback",
-									defaults: new { controller = "Home", action = "Index" } );
-			 } );
-		}
-	}
+        routes.MapSpaFallbackRoute(
+                name: "spa-fallback",
+                defaults: new { controller = "Home", action = "Index" } );
+      } );
+    }
+  }
 }

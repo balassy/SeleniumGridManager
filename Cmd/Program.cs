8456c2c0ccb29a7ab5435b9832f6c98e20ba5dfa
György Balássy <balassy@users.noreply.github.com>
2018-04-19 06:19:46 +0200

    FIXED: Second parameter was missing to start new process from the command line.

--- Cmd/Program.cs
+++ Cmd/Program.cs
@@ -10,45 +10,45 @@ using SeleniumGridManager.Lib.Services.Screenshot;
 
 namespace SeleniumGridManager.Cmd
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			CommandLine.Parser.Default.ParseArguments<CommandLineOptions>( args )
-				.WithParsed( Program.HandleRecognizedCommandLineOptions )
-				.WithNotParsed( Program.HandleUnrecognizedCommandLineOptions );
-		}
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      CommandLine.Parser.Default.ParseArguments<CommandLineOptions>( args )
+        .WithParsed( Program.HandleRecognizedCommandLineOptions )
+        .WithNotParsed( Program.HandleUnrecognizedCommandLineOptions );
+    }
 
-		private static void HandleRecognizedCommandLineOptions( CommandLineOptions opts )
-		{
-			if( !String.IsNullOrEmpty( opts.ScreenshotTargetFile ) )
-			{
-				Bitmap bmp = ScreenshotManager.CaptureDesktop();
-				bmp.Save( opts.ScreenshotTargetFile );
-			}
+    private static void HandleRecognizedCommandLineOptions( CommandLineOptions opts )
+    {
+      if( !String.IsNullOrEmpty( opts.ScreenshotTargetFile ) )
+      {
+        Bitmap bmp = ScreenshotManager.CaptureDesktop();
+        bmp.Save( opts.ScreenshotTargetFile );
+      }
 
-			if( opts.ListProcesses )
-			{
-				IEnumerable<Process> processes = ProcessManager.List();
-				foreach( Process p in processes )
-				{
-					Console.WriteLine( "{0}\t{1}", p.Id, p.MainWindowTitle );
-				}
-			}
+      if( opts.ListProcesses )
+      {
+        IEnumerable<Process> processes = ProcessManager.List();
+        foreach( Process p in processes )
+        {
+          Console.WriteLine( "{0}\t{1}", p.Id, p.MainWindowTitle );
+        }
+      }
 
-			if( opts.ProcessIdToKill != default(int) )
-			{
-				ProcessManager.Kill( opts.ProcessIdToKill );
-			}
+      if( opts.ProcessIdToKill != default( int ) )
+      {
+        ProcessManager.Kill( opts.ProcessIdToKill );
+      }
 
-			if( opts.StartNewProcess )
-			{
-				ProcessManager.Start(@"C:\Users\Selenium\Desktop\Startup\start_node.cmd");
-			}
-		}
+      if( opts.StartNewProcess )
+      {
+        ProcessManager.Start( @"C:\Users\Selenium\Desktop\Startup\start_node.cmd", String.Empty );
+      }
+    }
 
-		private static void HandleUnrecognizedCommandLineOptions( IEnumerable<Error> errors )
-		{
-		}
-	}
+    private static void HandleUnrecognizedCommandLineOptions( IEnumerable<Error> errors )
+    {
+    }
+  }
 }
